4d901bff7b8579b64d521458672057d79ce326d0
eugenie desaunai <eugeniedesaunai@gmail.com>
2022-09-07 11:19:00 +0200

    ajout commentaire

--- app/Models/Recette.php
+++ app/Models/Recette.php
@@ -9,33 +9,36 @@ class Recette extends Model // le "extends Model" permet d'aller chercher les é
 
 {
     use HasFactory;
-    public function images()
+    public function images() // la méthode "images" se transforme en variable/propriété 
+    //et être utiliser dans la vue ensuite
     { // hasMany ou hasOne : à une clé étrangère
-        return $this->hasMany(Image::class) ; 
-    } 
+        return $this->hasMany(Image::class);
+    }
 
-    public function etapes()
+    public function etapes() // la méthode "etapes" se transforme en variable/propriété 
+    //et être utiliser dans la vue ensuite
     { // hasMany ou hasOne : à une clé étrangère
-        return $this->hasMany(Etape::class) ;
-    } 
+        return $this->hasMany(Etape::class);
+    }
 
-    public function ingredients()
+    public function ingredients() // la méthode "ingredients" se transforme en variable/propriété 
+    //et être utiliser dans la vue ensuite
     { // hasMany ou hasOne : à une clé étrangère
-        return $this->hasMany(Ingredient::class) ; 
-    } 
+        return $this->hasMany(Ingredient::class);
+    }
 
     public function plat()
     { // belongsTo ou belongToMany : la  clé étrangère appartient à cette classe
         return $this->belongsTo(Plat::class); //quand on respecte la convention laravel
-    } 
+    }
 
     public function saison()
     { // belongsTo ou belongToMany : la  clé étrangère appartient à cette classe
         return $this->belongsTo(Saison::class); //quand on respecte la convention laravel
-    } 
+    }
 
     public function utilisateur()
     { // belongsTo ou belongToMany : la  clé étrangère appartient à cette classe
         return $this->belongsTo(Utilisateur::class); //quand on respecte la convention laravel
-    } 
+    }
 }
